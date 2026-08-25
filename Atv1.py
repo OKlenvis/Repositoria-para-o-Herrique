329049d566d99881591dd2cfa9b17f45c769739c
OKlenvis <kelvin.campos.fernandes@escola.pr.gov.br>
2026-08-25 10:43:36 +0000

    Fiz a atividade

---
+++ Atv1.py
@@ -0,0 +1,5 @@
+nome=str=input("Diga seu nome: ")
+sobrenome=str=input("Diga seu sobrenome: ")
+profissão=str=input("Diga sua profissão: ")
+
+print(f"Olá {nome} {sobrenome}, a sua profissão é {profissão}")
