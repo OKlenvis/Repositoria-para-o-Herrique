cbd70ddc833bb5eb9a03090bebd4fad0c53109a3
OKlenvis <kelvin.campos.fernandes@escola.pr.gov.br>
2026-08-25 11:12:53 +0000

    Fiz tudo

--- Atv2.py
+++ Atv2.py
@@ -1,4 +1,17 @@
+O=str(input("Escolh um operador (+, -, * ou /): "))
 n1=float(input("Primeiro número: "))
 n2=float(input("Sgundo Número: "))
 
-print(f"{n1+n2}, {n1-n2}, {n1*n2}, {n1/n2}")
+
+if O == '+':
+    print(n1+n2)
+
+elif O == "-":
+    print(n1-n2)
+
+elif O == "*":
+    print(n1*n2)
+
+else:
+    print(n1/n2)
+
